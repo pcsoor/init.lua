899e893cff1b8d0151f514364a2466985debeb1a
Peter Frank Csoor <peter.csoor@gmail.com>
2026-01-21 16:36:12 +0100

    minor config change

--- lua/pcsoor/lazy/nvim-tree.lua
+++ lua/pcsoor/lazy/nvim-tree.lua
@@ -11,7 +11,7 @@ return {
     require("nvim-tree").setup({
       sort_by = "case_sensitive",
       view = {
-        width = 30,
+        width = 40,
         side = "left",
       },
       renderer = {
